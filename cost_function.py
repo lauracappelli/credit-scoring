93ac5ebfa72839fddcf8032d00754427dd06e464
Laura Cappelli <lcappelli@fe.infn.it>
2025-07-09 14:33:25 +0200

    Update config & const_function followig the jupyter notebook

--- cost_function.py
+++ cost_function.py
@@ -4,8 +4,11 @@ import hybrid
 import math
 import time
 
-def one_class_const(Q, m, n, c):
-    # add penalty: "one counterpart per class"
+def one_class_const(m, n, mu=1):
+    Q = np.zeros([n*m, n*m])
+    c = 0
+
+    # penalty: "one counterpart per class"
     for ii in range(n):
         for jj in range(m):
             tt = ii*m+jj
@@ -17,110 +20,112 @@ def one_class_const(Q, m, n, c):
                 Q[tt][rr] += 1
                 Q[rr][tt] += 1
         c += 1
-    return (Q, c)
+    return (mu*Q, mu*c)
 
-def first_counterpart_const(Q, m):
-    # add penalty: "first counterpart in first class"
+def first_counterpart_const(m, n, mu=1):
+    Q = np.zeros([n*m, n*m])
+    
+    # penalty: "first counterpart in first class"
     for jj in range(1, m):
         Q[jj][jj] += 1
         Q[0][jj] -= 0.5
         Q[jj][0] -= 0.5
-    return Q
+    return mu*Q
 
-def last_counterpart_const(Q, n, m):
-    # add penalty: "last counterpart in the last class"
+def last_counterpart_const(m, n, mu=1):
+    Q = np.zeros([n*m, n*m])
+
+    # penalty: "last counterpart in the last class"
     for jj in range(m-1):
         tt = (n-1)*m+jj
         Q[tt][tt] += 1
         Q[(n*m)-1][tt] -= 0.5
         Q[tt][(n*m)-1] -= 0.5
-    return Q
+    return mu*Q
 
-def staircase_constr(Q, m, n):
+def staircase_constr(m, n, mu=1):
+    Q = first_counterpart_const(m,n) + last_counterpart_const(m,n)
 
-    Q = first_counterpart_const(Q, m)
-    Q = last_counterpart_const(Q, m, n)
-
-    # add penalty: "penalize not permitted submatrix"
+    # penalty: "penalize not permitted submatrix"
+    # a submatrix is
+    # [[x1, x1], [x3, x4]]
     for ii in range(n-1):
         for jj in range(m-1):
-            aa = ii*m+jj # x_{i,j}=x1
-            bb = aa+1   # x_{i,j+1}=x2
-            cc = (ii+1)*m+jj # x_{i+1,j}=x3
-            dd = cc+1 # x_{i+q,j+1}=x4
+            x1 = ii*m+jj
+            x2 = x1+1
+            x3 = (ii+1)*m+jj
+            x4 = x3+1
 
             # add linear terms
-            Q[aa][aa] += 1
-            Q[dd][dd] += 1
+            Q[x1][x1] += 1
+            Q[x4][x4] += 1
 
             # add quadratic terms
-            Q[aa][bb] += 0.5
-            Q[bb][aa] += 0.5
+            Q[x1][x2] += 0.5
+            Q[x2][x1] += 0.5
 
-            Q[aa][cc] -= 0.5
-            Q[cc][aa] -= 0.5
+            Q[x1][x3] -= 0.5
+            Q[x3][x1] -= 0.5
 
-            Q[aa][dd] -= 1
-            Q[dd][aa] -= 1
+            Q[x1][x4] -= 1
+            Q[x4][x1] -= 1
 
-            Q[bb][cc] += 0.5
-            Q[cc][bb] += 0.5
+            Q[x2][x3] += 0.5
+            Q[x3][x2] += 0.5
 
-            Q[bb][dd] -= 0.5
-            Q[dd][bb] -= 0.5
+            Q[x2][x4] -= 0.5
+            Q[x4][x2] -= 0.5
 
-            Q[cc][dd] += 0.5
-            Q[dd][cc] += 0.5
+            Q[x3][x4] += 0.5
+            Q[x4][x3] += 0.5
 
-    # add penalty: "penalize restarting from class 0"
+    # penalty: "penalize restarting from class 0"
     for ii in range(n-1):
-        aa = ii*m # x_{i,j}=x1
-        bb = aa+1   # x_{i,j+1}=x2
-        cc = (ii+1)*m # x_{i+1,j}=x3
-        dd = cc+1 # x_{i+q,j+1}=x4
+        x1 = ii*m
+        x2 = x1+1
+        x3 = (ii+1)*m
 
-        Q[cc][cc] += 1
+        Q[x3][x3] += 1
 
-        Q[aa][cc] -= 0.5
-        Q[cc][aa] -= 0.5
+        Q[x1][x3] -= 0.5
+        Q[x3][x1] -= 0.5
 
-        Q[bb][cc] -= 0.5
-        Q[cc][bb] -= 0.5
+        Q[x2][x3] -= 0.5
+        Q[x3][x2] -= 0.5
 
-        Q[aa][bb] += 0.5
-        Q[bb][aa] += 0.5
+        Q[x1][x2] += 0.5
+        Q[x2][x1] += 0.5
 
-    return Q
+    return mu*Q
 
-def concentration_constr(Q, m, n, c):
-    i2j1 = [] #(i_1,i_2,j)
-    u2 = [] #(u_1,u_2)
+def concentration_constr(m, n, mu=1):
+    Q = np.zeros([n*m, n*m])
+
+    u = np.zeros([n * n * m, 2], dtype=int)
+    index = 0
     for i1 in range(n):
         for i2 in range(n):
             for j in range(m):
-                i2j1.append([i1+1,i2+1,j+1])
-    for l in i2j1:
-        u_1=(l[0]-1)*m+l[2]
-        u_2=(l[1]-1)*m+l[2]
-        u2.append([u_1-1,u_2-1])
-        
-    # add initial cost function: "adjusted herfindhal index"
-    c += 1/(1-m)
-    gamma = m/(m-1)
-    for u_item in u2:
-        if u_item[0]==u_item[1]:
-            Q[u_item[0]][u_item[1]] += gamma
-        else:
-            Q[u_item[0]][u_item[1]] += gamma/2
+                u[index] = [(i1)*m+j, (i2)*m+j]
+                index += 1
 
-    return (Q, c)
+    # penalty: "concentration"
+    c = 1/(1-m)
+    gamma = m/(m-1)
+    for (u1, u2) in u:
+        if u1==u2:
+            Q[u1][u2] += gamma
+        else:
+            Q[u1][u2] += gamma/2
+
+    return (mu*Q, mu*c)
 
 def check_staircase(matrix):
 
     # check if each counterpart is in one class
     ones_per_row = np.sum(matrix == 1, axis=1)
     if not np.all(ones_per_row == 1):
-        print("Error: more than one class per counterpart")
+        print("Error: more or less than one class per counterpart")
         return False
 
     # retreive all the 1's indexes
@@ -144,12 +149,33 @@ def check_staircase(matrix):
             print(f"Error in the counterpart {i+2}")
             return False
 
+    print("Staircase matrix constraint checked")
     return True
 
-def check_concentration(matrix):
-    ones_per_column = np.sum(matrix == 1, axis=0)
+def check_concentration(matrix, m, n, alpha_conc = 0.05):
+    J_floor = math.floor(n*n*(alpha_conc + (1-alpha_conc)/m))
+    s = 0
+    for i1 in range(n):
+        for i2 in range(n):
+            for j in range(m):
+                s = s + matrix[i1,j] * matrix[i2,j]
+    if s <= J_floor:
+        print("Concentration constraint checked")
+        return True
+    else:
+        print("Error: concentration constraint not respected")
+        return True
 
-    print(ones_per_column)
+def check_concentration_approx(matrix):
+    ones_per_column = np.sum(matrix == 1, axis=0)
+    # print(ones_per_column)
+
+    if np.ptp(ones_per_column) <= 1:
+        print("Concentration (approx) constraint checked")
+        return True
+    else:
+        print("Error: concentration (approx) constraint not respected")
+        return True
 
 def from_matrix_to_bqm(matrix, c):
     
@@ -181,17 +207,24 @@ def main():
     config = read_config()
     n = config['n_counterpart']
     m = config['m_company']
+
+    alpha_conc = config['alpha_concentration']
     shots = config['shots']
 
-    # Iniatilize Q and c
-    Q = np.zeros([n*m, n*m])
-    c = 0
+    mu_one_calss_constr = config['mu_one_calss_constr']
+    mu_staircase_constr = config['mu_staircase_constr']
+    mu_concentration_constr = config['mu_concentration_constr']
+
+    # Gen Q matrix
+    start_time = time.perf_counter_ns()
+    (Q_one_class,c_one_class) = one_class_const(m,n,mu_one_calss_constr)
+    Q_staircase = staircase_constr(m,n,mu_staircase_constr)
+    (Q_conc,c_conc) = concentration_constr(m, n, mu_concentration_constr)
+
+    Q = Q_one_class + Q_staircase + Q_conc
+    c = c_one_class + c_conc
 
     # BQM generation
-    start_time = time.perf_counter_ns()
-    (Q,c) = one_class_const(Q,m,n,c)
-    # Q = staircase_constr(Q,m,n)
-    # (Q,c) = concentration_constr(Q,m,n,c)
     bqm = from_matrix_to_bqm(Q, c)
     end_time = time.perf_counter_ns()
     print(f"Matrix size:{m*n}*{m*n}")
@@ -207,23 +240,24 @@ def main():
     print(f"Time of annealing solution: {(end_time - start_time)/10e9} s\n")
 
     check_staircase(annealing_matrix)
-    # check_concentration(annealing_matrix)
+    check_concentration(annealing_matrix, m, n, alpha_conc)
+    check_concentration_approx(annealing_matrix)
 
     # solving exactly
-    start_time = time.perf_counter_ns()
-    e_result = exactSolver(bqm)
-    df_result = e_result.lowest().to_pandas_dataframe()
-    end_time = time.perf_counter_ns()
-    elapsed_time_ns = end_time - start_time
-    # print(f"\nALL Exact solutions:\n{df_result}")
-    # print first result
-    matrix = df_result.iloc[:, :m*n].to_numpy()
-    # print(f"First solution:\n{matrix[0].reshape(n, m)}")
-    # Print all the solutions
-    print(f"Exact solutions: {int(matrix.size/(m*n))}")
-    for sol in matrix[:]:
-        print(f"solution:\n{sol.reshape(n, m)}")
-    print(f"Time of all exact solutions: {elapsed_time_ns/10e9} s")
+    # start_time = time.perf_counter_ns()
+    # e_result = exactSolver(bqm)
+    # df_result = e_result.lowest().to_pandas_dataframe()
+    # end_time = time.perf_counter_ns()
+    # elapsed_time_ns = end_time - start_time
+    # # print(f"\nALL Exact solutions:\n{df_result}")
+    # # print first result
+    # matrix = df_result.iloc[:, :m*n].to_numpy()
+    # # print(f"First solution:\n{matrix[0].reshape(n, m)}")
+    # # Print all the solutions
+    # print(f"Exact solutions: {int(matrix.size/(m*n))}")
+    # for sol in matrix[:]:
+    #     print(f"solution:\n{sol.reshape(n, m)}")
+    # print(f"Time of all exact solutions: {elapsed_time_ns/10e9} s")
 
 
 if __name__ == '__main__':
