c8190ed01c4b48e8c48f084029048aa231c55838
Laura Cappelli <lcappelli@fe.infn.it>
2025-07-29 10:22:59 +0200

    Add concentration and threshold constraints in gurobi

--- gurobi-test.py
+++ gurobi-test.py
@@ -5,93 +5,10 @@ import hybrid
 import time
 import gurobipy as gpy
 from gurobipy import GRB
+from cost_function import *
 
-# staircase generation as numpy matrix
-def gen_Q_staircase(m,n):
-
-    # Iniatilize Q and c
-    Q = np.zeros([n*m, n*m])
-    c = 0
-
-    # add penalty: "first counterpart in first class"
-    for jj in range(1, m):
-        Q[jj][jj] += 1
-        Q[0][jj] -= 0.5
-        Q[jj][0] -= 0.5
-
-    # add penalty: "last counterpart in the last class"
-    for jj in range(m-1):
-        tt = (n-1)*m+jj
-        Q[tt][tt] += 1
-        Q[(n*m)-1][tt] -= 0.5
-        Q[tt][(n*m)-1] -= 0.5
-
-    # add penalty: "one counterpart per class"
-    for ii in range(n):
-        for jj in range(m):
-            tt = ii*m+jj
-            Q[tt][tt] += -1
-        for jj in range(m-1):
-            for kk in range(jj+1,m):
-                tt = ii*m+jj
-                rr = ii*m+kk
-                Q[tt][rr] += 1
-                Q[rr][tt] += 1
-        c += 1
-
-    # add penalty: "penalize not permitted submatrix"
-    for ii in range(n-1):
-        for jj in range(m-1):
-            aa = ii*m+jj # x_{i,j}=x1
-            bb = aa+1   # x_{i,j+1}=x2
-            cc = (ii+1)*m+jj # x_{i+1,j}=x3
-            dd = cc+1 # x_{i+q,j+1}=x4
-
-            # add linear terms
-            Q[aa][aa] += 1
-            Q[dd][dd] += 1
-
-            # add quadratic terms
-            Q[aa][bb] += 0.5
-            Q[bb][aa] += 0.5
-
-            Q[aa][cc] -= 0.5
-            Q[cc][aa] -= 0.5
-
-            Q[aa][dd] -= 1
-            Q[dd][aa] -= 1
-
-            Q[bb][cc] += 0.5
-            Q[cc][bb] += 0.5
-
-            Q[bb][dd] -= 0.5
-            Q[dd][bb] -= 0.5
-
-            Q[cc][dd] += 0.5
-            Q[dd][cc] += 0.5
-
-    # add penalty: "penalize restarting from class 0"
-    for ii in range(n-1):
-        aa = ii*m # x_{i,j}=x1
-        bb = aa+1   # x_{i,j+1}=x2
-        cc = (ii+1)*m # x_{i+1,j}=x3
-        dd = cc+1 # x_{i+q,j+1}=x4
-
-        Q[cc][cc] += 1
-
-        Q[aa][cc] -= 0.5
-        Q[cc][aa] -= 0.5
-
-        Q[bb][cc] -= 0.5
-        Q[cc][bb] -= 0.5
-
-        Q[aa][bb] += 0.5
-        Q[bb][aa] += 0.5
-
-    return (Q, c)
-
-def gurobi_solver(m, n, matrix, c):
-    size = n * m
+def gurobi_solver(m, n, matrix, c, g_n_sol, g_fidelity):
+    size = matrix.shape[0]
     # model definition
     qubo_model = gpy.Model("QCS")
     qubo_vars = qubo_model.addVars(size, vtype=GRB.BINARY, name="x")
@@ -112,11 +29,11 @@ def gurobi_solver(m, n, matrix, c):
     # qubo_model.setParam("TimeLimit", timelimit)
     
     # Search more than 1 solution
-    num_max_solutions = 10
+    num_max_solutions = g_n_sol
     if num_max_solutions > 1:
         qubo_model.setParam("PoolSolutions", num_max_solutions)
         qubo_model.setParam("PoolSearchMode", 2)
-        # qubo_model.setParam("PoolGap", 0.1) # 10% fidelity
+        qubo_model.setParam("PoolGap", g_fidelity)
 
     # Run the Gurobi QUBO optimization
     qubo_model.optimize()
@@ -125,6 +42,8 @@ def gurobi_solver(m, n, matrix, c):
     if qubo_model.Status in {GRB.OPTIMAL, GRB.SUBOPTIMAL}:
         if num_max_solutions == 1:
             solution = [int(qubo_vars[i].X) for i in range(size)]
+            if len(solution) > m*n:
+                solution = solution[:m*n]
             print("\nBest solution:\n", np.array(solution).reshape(n, m))
             print("Cost of the function:", qubo_model.ObjVal)
         else:
@@ -136,17 +55,54 @@ def gurobi_solver(m, n, matrix, c):
                 qubo_bitstring = np.array(
                     [int(qubo_vars[jj].Xn) for jj in range(size)]
                 )
+                if qubo_bitstring.shape[0] > m*n:
+                    qubo_bitstring = qubo_bitstring[:m*n]
                 print(f"solution {sol_idx+1}:\n{qubo_bitstring.reshape(n,m)}")
                 print("Cost of the function:", qubo_model.PoolObjVal)
     else:
         print("No solutions found")
 
 def main():
-    m = 3
-    n = 8
+    config = read_config()
+    n = config['n_counterpart']
+    m = config['m_company']
 
-    matrix, c = gen_Q_staircase(m, n)
-    gurobi_solver(m, n, matrix, c)
+    alpha_conc = config['alpha_concentration']
+    shots = config['shots']
+
+    mu_one_calss_constr = config['mu']['one_calss']
+    mu_staircase_constr = config['mu']['logic']
+    mu_concentration_constr = config['mu']['concentration']
+    mu_min_thr_constr = config['mu']['min_thr']
+    mu_max_thr_constr = config['mu']['max_thr']
+
+    #-------------------------------
+
+    # Gen Q matrix
+    Q = np.zeros([m*n, m*n])
+    c = 0
+    if config['constraints']['one_class'] == True:
+        (Q_one_class,c_one_class) = one_class_const(m,n,mu_one_calss_constr)
+        Q = Q + Q_one_class
+        c = c + c_one_class
+    if config['constraints']['logic'] == True:
+        Q = Q + staircase_constr(m,n,mu_staircase_constr)
+    if config['constraints']['conentration'] == True:
+        (Q_conc,c_conc) = concentration_constr(m, n, mu_concentration_constr)
+        Q = Q + Q_conc
+        c = c + c_conc
+    if config['constraints']['min_thr'] == True:
+        (Q_min_thr, c_min_thr) = threshold_constr(m, n, Q.shape[0], 'min', mu_min_thr_constr)
+        pad = Q_min_thr.shape[0] - Q.shape[0]
+        Q = np.pad(Q, pad_width=((0,pad), (0, pad)), mode='constant', constant_values=0) + Q_min_thr
+        c = c + c_min_thr
+    if config['constraints']['max_thr'] == True:
+        (Q_max_thr, c_max_thr) = threshold_constr(m, n, Q.shape[0], 'max', mu_max_thr_constr)
+        pad = Q_max_thr.shape[0] - Q.shape[0]
+        Q = np.pad(Q, pad_width=((0,pad), (0, pad)), mode='constant', constant_values=0) + Q_max_thr
+        c = c + c_max_thr
+
+    gurobi_solver(m, n, Q, c, config['gurobi_n_sol'], config['gurobi_fidelity'])
 
 if __name__ == "__main__":
     main()
