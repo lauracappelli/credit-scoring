6c49d6d8aac3dd29ce4faa62161501226abd24f5
Laura Cappelli <lcappelli@fe.infn.it>
2025-07-31 17:39:21 +0200

    Add messages for heterogeneous constraint check + Improve readability

--- src/check_constraints.py
+++ src/check_constraints.py
@@ -8,8 +8,8 @@ def check_staircase(matrix, verbose=False):
     ones_per_row = np.sum(matrix == 1, axis=1)
     if not np.all(ones_per_row == 1):
         if verbose:
-            print("Error: logic constraint not respected")
-            print("\tMmore or less than one class per counterpart")
+            print("\tx Error: logic constraint not respected")
+            print("\t\tMore or less than one class per counterpart")
         return False
 
     # retreive all the 1's indexes
@@ -19,13 +19,13 @@ def check_staircase(matrix, verbose=False):
     # check the first and the last counterpart
     if index_1[0] != 0:
         if verbose:
-            print("Error: logic constraint not respected")
-            print("\tError in the first counterpart")
+            print("\tx Error: logic constraint not respected")
+            print("\t\tError in the first counterpart")
         return False
     if index_1[-1] != matrix.shape[1]-1:
         if verbose:
-            print("Error: logic constraint not respected")
-            print("\tError in the last counterpart")
+            print("\tx Error: logic constraint not respected")
+            print("\t\tError in the last counterpart")
         return False
 
     # check if the matrix is a staircase matrix
@@ -35,12 +35,12 @@ def check_staircase(matrix, verbose=False):
         # print(f"index {i+1} contains {el}")
         if el != index_1[i] and el != index_1[i]+1:
             if verbose:
-                print("Error: logic constraint not respected")
-                print(f"\tError in the counterpart {i+2}")
+                print("\tx Error: logic constraint not respected")
+                print(f"\t\tError in the counterpart {i+2}")
             return False
 
     if verbose:
-        print("Staircase matrix constraint checked")
+        print("\t\u2713 Logic constraint checked")
     return True
 
 def check_concentration(matrix, m, n, alpha_conc = 0.05, verbose=False):
@@ -52,11 +52,11 @@ def check_concentration(matrix, m, n, alpha_conc = 0.05, verbose=False):
                 s = s + matrix[i1,j] * matrix[i2,j]
     if s <= J_floor:
         if verbose:
-            print("Concentration constraint checked")
+            print("\t\u2713 Concentration constraint checked")
         return True
     else:
         if verbose:
-            print("Error: concentration constraint not respected")
+            print("\tx Error: concentration constraint not respected")
         return False
 
 def check_concentration_approx(matrix, verbose=False):
@@ -65,11 +65,11 @@ def check_concentration_approx(matrix, verbose=False):
 
     if np.ptp(ones_per_column) <= 1:
         if verbose:
-            print("Concentration (approx) constraint checked")
+            print("\t\u2713 Concentration (approx) constraint checked")
         return True
     else:
         if verbose:
-            print("Error: concentration (approx) constraint not respected")
+            print("\tx Error: concentration (approx) constraint not respected")
         return False
 
 def check_upper_thrs(matrix, max_thrs, verbose=False):
@@ -77,11 +77,11 @@ def check_upper_thrs(matrix, max_thrs, verbose=False):
     for ii in np.sum(matrix, axis=0):
         if ii > max_thrs:
             if verbose:
-                print("Error: upper threshold limit constraint not respected")
+                print("\tx Error: upper threshold limit constraint not respected")
             return False
     
     if verbose:
-        print("Upper threshold limit constraint checked")
+        print("\t\u2713 Upper threshold limit constraint checked")
     return True
 
 def check_lower_thrs(matrix, min_thrs, verbose=False):
@@ -89,16 +89,15 @@ def check_lower_thrs(matrix, min_thrs, verbose=False):
     for ii in np.sum(matrix, axis=0):
         if ii < min_thrs:
             if verbose:
-                print("Error: lower threshold limit constraint not respected")
+                print("\tx Error: lower threshold limit constraint not respected")
             return False
     
     if verbose:
-        print("Lower threshold limit constraint checked")
+        print("\t\u2713 Lower threshold limit constraint checked")
     return True
 
 def check_heterogeneity(matrix, dr, verbose=False):
-    print(f"default: {dr.T}")
-
+    # print(f"default: {dr.T}")
     grad_cardinality = np.sum(matrix, axis=0) #N_j
     grad_dr = np.sum(matrix * dr, axis=0) / grad_cardinality #l_j
     binomial_var = stats.binom.var(1, grad_dr) #sigma^2_j senza dividere per n (altrimenti si sostituisce grad_cardinality a 1)
@@ -120,9 +119,15 @@ def check_heterogeneity(matrix, dr, verbose=False):
         pooled_std_dev = np.sqrt(((n1 - 1)*s1 + (n2 - 1)*s2) / (n1 + n2 - 2))
         t_stat[i] = (grad_dr[i] - grad_dr[i+1]) / (pooled_std_dev * np.sqrt(1/n1 + 1/n2))
         p_val[i] = 2 * stats.t.sf(np.abs(t_stat[i]), n1 + n2 - 2)
+        if p_val[i] > 0.01:
+            if verbose:
+                print("\tx Error: heterogeneous constraint not respected")
+                return False
 
-    print("t-test", t_stat)
-    print("p_val", p_val)
+    # print("t-test", t_stat)
+    # print("p_val", p_val)
 
+    if verbose:
+        print("\t\u2713 Heterogeneous constraint checked")
     return True
 
