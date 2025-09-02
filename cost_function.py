b6f5da9b2738e2c64456f88592058fd089f4dd27
Laura Cappelli <lcappelli@fe.infn.it>
2025-09-02 10:03:03 +0200

    Update cost function file following the notebook

--- cost_function.py
+++ cost_function.py
@@ -8,11 +8,32 @@ import itertools
 import gurobipy as gpy
 from gurobipy import GRB
 
+# penalty: "first counterpart in first class"
+def first_counterpart_const(m, n, mu=1):
+    Q = np.zeros([n*m, n*m])
+    
+    for jj in range(1, m):
+        Q[jj][jj] += 1
+        Q[0][jj] -= 0.5
+        Q[jj][0] -= 0.5
+    return mu*Q
+
+# penalty: "last counterpart in the last class"
+def last_counterpart_const(m, n, mu=1):
+    Q = np.zeros([n*m, n*m])
+
+    for jj in range(m-1):
+        tt = (n-1)*m+jj
+        Q[tt][tt] += 1
+        Q[(n*m)-1][tt] -= 0.5
+        Q[tt][(n*m)-1] -= 0.5
+    return mu*Q
+
+# penalty: "one class per counterpart"
 def one_class_const(m, n, mu=1):
     Q = np.zeros([n*m, n*m])
     c = 0
 
-    # penalty: "one counterpart per class"
     for ii in range(n):
         for jj in range(m):
             tt = ii*m+jj
@@ -26,34 +47,15 @@ def one_class_const(m, n, mu=1):
         c += 1
     return (mu*Q, mu*c)
 
-def first_counterpart_const(m, n, mu=1):
-    Q = np.zeros([n*m, n*m])
-    
-    # penalty: "first counterpart in first class"
-    for jj in range(1, m):
-        Q[jj][jj] += 1
-        Q[0][jj] -= 0.5
-        Q[jj][0] -= 0.5
-    return mu*Q
-
-def last_counterpart_const(m, n, mu=1):
-    Q = np.zeros([n*m, n*m])
-
-    # penalty: "last counterpart in the last class"
-    for jj in range(m-1):
-        tt = (n-1)*m+jj
-        Q[tt][tt] += 1
-        Q[(n*m)-1][tt] -= 0.5
-        Q[tt][(n*m)-1] -= 0.5
-    return mu*Q
-
+# penalty: "staircase matrix"
 def staircase_constr(m, n, mu=1):
     Q = first_counterpart_const(m,n) + last_counterpart_const(m,n)
 
-    # penalty: "penalize not permitted submatrix"
-    # a submatrix is
+    # penalize not permitted submatrix, where a submatrix is
     # [[x1, x1], [x3, x4]]
     for ii in range(n-1):
+
+        # penalize: [[1 0],[0 0]], [[0 0],[0 1]], [[0 1],[1 0]]
         for jj in range(m-1):
             x1 = ii*m+jj
             x2 = x1+1
@@ -83,8 +85,7 @@ def staircase_constr(m, n, mu=1):
             Q[x3][x4] += 0.5
             Q[x4][x3] += 0.5
 
-    # penalty: "penalize restarting from class 0"
-    for ii in range(n-1):
+        # penalize restarting from class 0
         x1 = ii*m
         x2 = x1+1
         x3 = (ii+1)*m
@@ -101,28 +102,6 @@ def staircase_constr(m, n, mu=1):
         Q[x2][x1] += 0.5
 
     return mu*Q
-
-def concentration_constr(m, n, mu=1):
-    Q = np.zeros([n*m, n*m])
-
-    u = np.zeros([n * n * m, 2], dtype=int)
-    index = 0
-    for i1 in range(n):
-        for i2 in range(n):
-            for j in range(m):
-                u[index] = [i1*m+j, i2*m+j]
-                index += 1
-
-    # penalty: "concentration"
-    c = 1/(1-m)
-    gamma = m/(m-1)
-    for (u1, u2) in u:
-        if u1==u2:
-            Q[u1][u2] += gamma
-        else:
-            Q[u1][u2] += gamma/2
-
-    return (mu*Q, mu*c)
 
 def monotonicity_constr(m, n, default, offset, mu1=1, mu2=1, mu3=1):
 
@@ -273,20 +252,40 @@ def monotonicity_constr(m, n, default, offset, mu1=1, mu2=1, mu3=1):
 
     return Q
 
+def concentration_constr(m, n, mu=1):
+    Q = np.zeros([n*m, n*m])
+
+    # penalty: "concentration"
+    c = 1/(1-m)
+    gamma = m/(m-1)
+    
+    for i1 in range(n):
+        for i2 in range(n):
+            for j in range(m):
+                u1 = (i1)*m+j
+                u2 = (i2)*m+j
+                if u1==u2:
+                    Q[u1][u2] += gamma
+                else:
+                    Q[u1][u2] += gamma/2
+
+    return (mu*Q, mu*c)
+
 def compute_lower_thrs(n):
     return math.floor(n*0.01) if math.floor(n*0.01) != 0 else 1
 
 def compute_upper_thrs(n, grades):
     return math.floor(n*0.15) if grades > 7 and math.floor(n*0.15) != 0 else (n-grades+1)
-
+    
 def threshold_constr(m, n, offset, minmax, mu=1):
 
+    # compute the thresholds
     if minmax == 'min':
         thr = compute_lower_thrs(n)
-        slack_vars = math.floor(1+math.log2(n-thr)) # to check
+        slack_vars = math.floor(1+math.log2(n-thr))
     elif minmax == 'max':
         thr = compute_upper_thrs(n, m)
-        slack_vars = math.floor(1+math.log2(thr)) # to check
+        slack_vars = math.floor(1+math.log2(thr))
     else:
         print("Error in threshold function call")
         sys.exit(1)
@@ -296,39 +295,37 @@ def threshold_constr(m, n, offset, minmax, mu=1):
     Q = np.zeros([dim, dim])
     c = m * thr * thr
 
+    # quadratic term (in the variable x)
     for i1 in range(n):
         for i2 in range(n):
             for j in range(m):
                 u2 = [i1*m+j, i2*m+j]
-                if u2[0]==u2[1]: # questo l'ho modificato, forse c'era un typo
-                    Q[u2[0]][u2[1]] += 1
-                else:
-                    Q[u2[0]][u2[1]] += 0.5
-                    Q[u2[1]][u2[0]] += 0.5
+                Q[u2[0]][u2[1]] += 0.5
+                Q[u2[1]][u2[0]] += 0.5
 
+    # quadratic term (in the slack variable)
     for l1 in range(slack_vars):
         for l2 in range(slack_vars):
             for j in range(m):
                 v2 = [l1*m+j, l2*m+j]
                 tmp = math.pow(2,math.floor((v2[0]+1)/m)+math.floor((v2[1]+1)/m))
-                if v2[0]==v2[1]:
-                    Q[offset+v2[0]][offset+v2[1]] += tmp
-                else:
-                    Q[offset+v2[0]][offset+v2[1]] += 0.5*tmp
-                    Q[offset+v2[1]][offset+v2[0]] += 0.5*tmp
+                Q[offset+v2[0]][offset+v2[1]] += 0.5*tmp
+                Q[offset+v2[1]][offset+v2[0]] += 0.5*tmp
 
-
+    # linear term (in the x variable)
     for i in range(n):
         for j in range(m):
             u = i*m+j
             Q[u,u] -= 2*thr
 
+    # linear term (in the slack variable)
     index = 0
     for l in range(slack_vars):
         for j in range(m):
             Q[offset+index][offset+index] += thr*math.pow(2,1+math.floor((l*m+j+1)/m))
             index += 1
 
+    # quadratic term (in the variables x and s)
     for i in range(n):
         for l in range(slack_vars):
             for j in range(m):
@@ -338,31 +335,6 @@ def threshold_constr(m, n, offset, minmax, mu=1):
                 Q[offset+w2[1]][w2[0]] -= -0.5*tmp
 
     return (mu*Q, mu*c)
-
-def from_matrix_to_bqm(matrix, c):
-    
-    Q_dict = {(i, j): matrix[i, j] for i in range(matrix.shape[0]) for j in range(matrix.shape[1])}# if matrix[i, j] != 0}
-    #print(Q_dict)
-    bqm = dimod.BinaryQuadraticModel.from_qubo(Q_dict, c)
-
-    return bqm
-
-def annealer_solver(dim, bqm, shots):
-
-    # Set up the sampler with an initial state
-    sampler = hybrid.samplers.SimulatedAnnealingProblemSampler(num_sweeps=shots)
-    state = hybrid.core.State.from_sample({i: 0 for i in range(dim)}, bqm)
- 
-    # Sample the problem
-    new_state = sampler.run(state).result()
- 
-    return new_state
-
-def exact_solver(bqm):
-    sampler = dimod.ExactSolver()
-    sampleset = sampler.sample(bqm)
-
-    return sampleset
 
 def brute_force_solver(Q, c, dim):
 
@@ -386,6 +358,31 @@ def brute_force_solver(Q, c, dim):
     #         Ymin = Y.copy()
 
     return (np.array(Ymin), Cmin)
+
+def from_matrix_to_bqm(matrix, c):
+    
+    Q_dict = {(i, j): matrix[i, j] for i in range(matrix.shape[0]) for j in range(matrix.shape[1])}# if matrix[i, j] != 0}
+    bqm = dimod.BinaryQuadraticModel.from_qubo(Q_dict, c)
+
+    return bqm
+
+def exact_solver(bqm):
+    
+    sampler = dimod.ExactSolver()
+    sampleset = sampler.sample(bqm)
+
+    return sampleset
+
+def annealer_solver(dim, bqm, shots):
+
+    # define the initial state (all elements = 0 or random elements)
+    state = hybrid.core.State.from_sample({i: 0 for i in range(dim)}, bqm)
+    # state = hybrid.core.State.from_sample({i: np.random.randint(0, 2) for i in range(dim)}, bqm)
+
+    sampler = hybrid.samplers.SimulatedAnnealingProblemSampler(num_sweeps=shots)
+    result_state = sampler.run(state).result()
+ 
+    return result_state
 
 def gurobi_solver(m, n, matrix, c, gurobi_n_sol, gurobi_fidelity):
     size = matrix.shape[0]
@@ -451,6 +448,7 @@ def main():
 
     config = read_config()
 
+    # generate a random dataset or select data from the dataset
     dataset = generate_data(config) if config['random_data'] == 'yes' else load_data(config)
     n = len(dataset)
     m = config['grades']
@@ -470,7 +468,7 @@ def main():
 
     #-------------------------------
 
-    # Gen Q matrix
+    # generate the appropriate Q matrix
     start_time = time.perf_counter_ns()
     Q = np.zeros([m*n, m*n])
     c = 0
@@ -498,10 +496,11 @@ def main():
         pad = Q_max_thr.shape[0] - Q.shape[0]
         Q = np.pad(Q, pad_width=((0,pad), (0, pad)), mode='constant', constant_values=0) + Q_max_thr
         c = c + c_max_thr
-
-    # BQM generation
-    bqm = from_matrix_to_bqm(Q, c)
     end_time = time.perf_counter_ns()
+
+    # generate the BMQ
+    bqm = from_matrix_to_bqm(Q, c)
+
     print(f"Matrix size:{Q.shape}")
     print(f"Time of generation: {(end_time - start_time)/10e9} s")
 
@@ -550,8 +549,9 @@ def main():
     # gurobi_sol = gurobi_solver(m, n, Q, c, config['gurobi_n_sol'], config['gurobi_fidelity'])
 
     #-------------------------------
+    print("Result validation:")
     verbose = True
-    test_one_solution(annealing_matrix, config, n, m, default, compute_upper_thrs(n,m), compute_lower_thrs(n), verbose)
+    is_valid = test_one_solution(annealing_matrix, config, n, m, default, compute_upper_thrs(n,m), compute_lower_thrs(n), verbose)
 
 if __name__ == '__main__':
     main()
