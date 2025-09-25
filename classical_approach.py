495170ef5693e75bcdf1d10d50079515012d5e57
lcappelli <lcappelli@fe.infn.it>
2025-09-25 17:31:34 +0200

    Update classical approach
    * Do not perform heterogeneity and homogeneity constraints if the number of counterparts is not sufficient
    * Improve print solutions

--- classical_approach.py
+++ classical_approach.py
@@ -9,11 +9,32 @@ def main():
 
     # Read iperparameters from config file
     config = read_config()
-    dataset = generate_or_load_dataset(config)# if config['random_data'] == 'yes' else load_data(config)
-    n = len(dataset)
+    dataset = generate_or_load_dataset(config)
+    n = config['n_counterpart']
     grades = config['grades']
     default = dataset['default'].to_numpy().reshape(n,1)
 
+    test_logic = config['test']['logic']
+    test_monotonicity = config['test']['monotonicity']
+    test_heterogeneity = config['test']['heterogeneity']
+    test_concentration = config['test']['concentration']
+    test_min_thr = config['test']['min_thr']
+    test_max_thr = config['test']['max_thr']
+    test_homogeneity = config['test']['homogeneity']
+
+    print("\nSELECTED INSTANCE:")
+    print("Number of counterparts: ", n)
+    print("Number of grades: ", grades)
+    print(f"The number of default is {int(np.sum(default))}")
+    print("Dataset:")
+    print(dataset.reset_index(drop=True))
+
+    if test_heterogeneity == True or test_homogeneity == True and n < grades * 30:
+        print("\nNumber of counterpars not sufficient for homogeneity and heterogeneity constraints.")
+        print("These two tests won't be performed.\n")
+        test_heterogeneity = False
+        test_homogeneity = False
+        
     # compute lower and upper thresholds:
     #  - lower threshold: 1% of the counterparts
     #    (or 1 if there are less than 100 counterparts)
@@ -27,7 +48,7 @@ def main():
     # list of valid solutions
     valid_solutions = []
 
-    print(f"\nTesting {grades ** n} combinations With the default vector:\n {default.T}")
+    print(f"Testing {grades ** n} combinations...")
 
     start_time = time.perf_counter_ns()    
 
@@ -45,19 +66,19 @@ def main():
         #  - previous tests did not fail
         # If a test fails, set the flag variable to False to avoid running other tests on the same combination
         flag = True
-        if config['test']['logic'] and flag and not check_staircase(matrix):
+        if test_logic and flag and not check_staircase(matrix):
             flag = False
-        if config['test']['concentration'] and flag and not check_concentration(matrix, config['alpha_concentration']):
+        if test_concentration and flag and not check_concentration(matrix, config['alpha_concentration']):
             flag = False
-        if config['test']['min_thr'] and flag and not check_upper_thrs(matrix, max_thr):
+        if test_min_thr and flag and not check_upper_thrs(matrix, max_thr):
             flag = False
-        if config['test']['max_thr'] and flag and not check_lower_thrs(matrix, min_thr):
+        if test_max_thr and flag and not check_lower_thrs(matrix, min_thr):
             flag = False
-        if config['test']['heterogeneity'] and flag and not check_heterogeneity(matrix, default, config['alpha_heterogeneity']):
+        if test_heterogeneity and flag and not check_heterogeneity(matrix, default, config['alpha_heterogeneity']):
             flag = False
-        if config['test']['homogeneity'] and flag and not check_homogeneity(matrix, default, config['alpha_homogeneity']):
+        if test_homogeneity and flag and not check_homogeneity(matrix, default, config['alpha_homogeneity']):
             flag = False
-        if config['test']['monotonicity'] and flag and not check_monotonicity(matrix, default):
+        if test_monotonicity and flag and not check_monotonicity(matrix, default):
             flag = False    
 
         # If all the tests are completed, the solution is valid
@@ -66,7 +87,11 @@ def main():
 
     end_time = time.perf_counter_ns()
     print(f"{len(valid_solutions)} solutions found in {(end_time-start_time)/10e9} s")
-    print(f"Solutions:\n{valid_solutions}\n")
+    if len(valid_solutions) > 0:
+        print(f"Solutions:")
+        for i, sol in enumerate(valid_solutions):
+            dataset["sol_" + str(i+1)] = np.array(sol)+1
+        print(dataset)
 
 if __name__ == '__main__':
     main()
