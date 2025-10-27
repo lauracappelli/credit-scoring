84de3de1a51f2009ecfdc90c1a8c204012fc09da
lauracappelli <lauracapp94@gmail.com>
2025-10-27 18:22:07 +0100

    Fix bash file for classic test
    Read the iperparameter from a csv with n, m and default vectors

--- classic_test.sh
+++ classic_test.sh
@@ -1,38 +1,37 @@
 #!/bin/bash
 
-file="config.yaml"
+param_file="output/classic_test/parameter.csv"
+config_file="config.yaml"
 
-# counterparts=(8 12 14 17 20 25 32* 40* 48 52)
-# grades=(3 3 4 4 5 5 5* 6* 6 6)
-counterparts=(32 40)
-grades=(5 6)
+while IFS= read -r line; do
 
-for i in "${!counterparts[@]}"; do
-    n=${counterparts[i]}
-    m=${grades[i]}
+    n=$(echo "$line" | awk -F',' '{print $1}')
+    m=$(echo "$line" | awk -F',' '{print $2}')
 
-    sed -i "19s/.*/n_counterpart: $n/" "$file"
-    sed -i "20s/.*/grades: $m/" "$file"
+    defs=($(echo "$line" | grep -oP '\[[^\]]*\]'))
+    def1=${defs[0]}
+    def2=${defs[1]}
+    def3=${defs[2]}
 
-    for run in {1..3}; do
+    echo "n: $n, m: $m, Default: $def1, $def2, $def3"
 
-        config_copy="output/classic_test/test2/config_${n}_${m}_run${run}.yaml"
-        cp "$file" "$config_copy"
+    sed -i "19s/.*/n_counterpart: $n/" "$config_file"
+    sed -i "20s/.*/grades: $m/" "$config_file"
 
-        def1=$((n-1))
-        def2=$((n-run))
-        
-        if [ "$n" -gt 39 ]; then
-            def3=$((n-6))
-            sed -i "26s/\[.*\]/[$def3,$def2,$def1]/" "$config_copy"
-        else
-            sed -i "26s/\[.*\]/[$def2,$def1]/" "$config_copy"
-        fi
-        
-        echo "Running test n=$n, m=$m, run=$run"
-        python classical_approach.py "$config_copy" > "output/classic_test/test2/output_${n}_${m}_run${run}.txt" 2>&1 &
+    default=($def1 $def2 $def3)
+
+    for run in {0..2}; do
+        run_id=$run+1
+        config_copy="output/classic_test/config_${n}_${m}_run$(($run+1)).yaml"
+        cp "$config_file" "$config_copy"
+        one_indices="one_indices: ${default[run]}"
+        sed -i "26s|.*|$one_indices|" "$config_copy"
+
+        echo "n: $n, m: $m, run $((run+1)), default=${default[run]}"
+        python classical_approach.py "$config_copy" > "output/classic_test/output_${n}_${m}_run${run}.txt" 2>&1 &
     done
     wait
-done
 
-rm -f output/classic_test/test2/config_*_run*.yaml
+done < "$param_file"
+
+rm -f output/classic_test/config_*_run*.yaml
