952ac3c890fbc1201a99c4533c2e41ae448f947f
lauracappelli <lauracapp94@gmail.com>
2025-11-07 16:28:23 +0100

    Add interpolation in classical data analysis

--- classic_data_analysis.py
+++ classic_data_analysis.py
@@ -46,5 +46,22 @@ def main():
     plt.title('Classical approach - Time vs Problem Size')
     plt.savefig("output/classic_test/classic-plot.png")
 
+    # Linear regression on log data
+    # test_time = a * problem_size^b
+    # in log scale:
+    # log(test_time) = log(a) + b * log(problem_size)
+    log_x = np.log(test_size)
+    log_y = np.log(test_time)
+    b, log_a = np.polyfit(log_x, log_y, 1)
+    a = np.exp(log_a)
+    print(f"Model: time = {a:.3e} * problem_size^{b:.3f}")
+
+    # Interpolation:
+    n_int=200
+    m_int=9
+    x_pred = comb(n_int-1, m_int-1) #15_000_000
+    y_pred = a * x_pred**b
+    print(f"With n={n_int}, m={m_int}, problem_size={x_pred:,} -> test_time ≈ {y_pred/3600:.3f} h")
+   
 if __name__ == '__main__':
     main()
