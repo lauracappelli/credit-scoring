c57ae59fa4bb96dd365fa786e5a6ff115acef519
“Patri-phy” <patrizio.spada7@gmail.com>
2026-05-21 17:36:41 +0200

    Test with gurobi

--- qubo_data_analysis.py
+++ qubo_data_analysis.py
@@ -14,38 +14,41 @@ def main():
     qubo_size = []
     energies = []
     correct = []
+    time = []
 
     for row in df.itertuples(index=False):
         n = row.n
         m = row.m
-        shots = row.shots
 
         for i in range(3):
             # First test
-            filename_test1 = f"output/qubo_test/test1_{n}_{m}_run{i}.txt"
+            filename_test1 = f"output/qubo_test/test6_{n}_{m}_shots_run{i}.txt"
             if os.path.isfile(filename_test1):
                 with open(filename_test1, "r") as f:
                     # Find the QUBO size
-                    for line in f:
-                        match_qubo_size = re.search(r"The QUBO problem has (\d+) variables", line)
-                        if match_qubo_size:
-                            qubo_size.append(int(match.group(1)))
-                            break
-                    # Find all energies and if the solutions are correct
+                    # for line in f:                        
+                    #     match_qubo_size = re.search(r"The QUBO problem has (\d+) variables", line)
+                    #     print(match_qubo_size)
+                    #     # if match_qubo_size:
+                    #     #     qubo_size.append(int(match.group(1)))
+                    #     #     break
+                    # # Find all energies and if the solutions are correct
                     text = f.read()
-                    energies.append([float(x) for x in re.findall(r"Energy:\s*([-\d\.eE]+)", text)])
-                    correct.append([x == "True" for x in re.findall(r"The solution is correct:\s*(True|False)", text)])
+                    time.append([float(x) for x in re.findall(r"Time to compute the solution:\s*(\d+\.\d+)\s*s", text)])
+                    # energies.append([float(x) for x in re.findall(r"Energy:\s*([-\d\.eE]+)", text)])
+                    # correct.append([x == "True" for x in re.findall(r"The solution is correct:\s*(True|False)", text)])
             
             # Second test
-            filename_test2 = f"output/qubo_test/test2_{n}_{m}_shots{shots}_run{i}.txt"
-            if os.path.isfile(filename_test2):
-                with open(filename_test2, "r") as f:
+            # filename_test2 = f"output/qubo_test/test2_{n}_{m}_shots{shots}_run{i}.txt"
+            # if os.path.isfile(filename_test2):
+            #     with open(filename_test2, "r") as f:
                     # Cerca: dimensione problema, costo della soluzione migliore (o costo medio), numero di sweep
 
     # Verifica
     print("QUBO size:", qubo_size)
-    print("Energy:", energies)
-    print("Solution correctness:", correct)
+    print("Time:", time)
+    # print("Energy:", energies)
+    # print("Solution correctness:", correct)
 
 if __name__ == '__main__':
     main()
--- qubo_test.sh
+++ qubo_test.sh
@@ -13,7 +13,7 @@ while IFS=";" read -r n m def1 def2 def3 sweeps; do
 
     for run in {0..2}; do
         # copy config file
-        config_copy="output/qubo_test/config_${n}_${m}_shots${sweeps}_run${run}.yaml"
+        config_copy="output/new_qubo_test/config_${n}_${m}_run${run}.yaml"
         cp "$config_file" "$config_copy"
         
         # compute hyperparameters
@@ -22,15 +22,132 @@ while IFS=";" read -r n m def1 def2 def3 sweeps; do
         mu_mon=$((def_number * 5))
         mu_conc=$((n / m * 10))
         mu_thr=$((mu_conc / 2))
-       
-        mu_1c=$(((n*m)**2))
+
+        # TEST 1 
+        # mu_1c=$(((n*m)**2))
+        # mu_fst_lst=$(((n*m)*5))
+        # mu_subm_1000=$(((n*m)*5))
+        # mu_subm_0001=$(((n*m)*5))
+        # mu_subm_0110=$(((n*m)*5))
+        # mu_restart=$(((n*m)*10))
+        # mu_glob_col=$(((n*m)*6))
+        # mu_change_class=$(((n*m)*6))
+
+        # # TEST 2 
+        # mu_1c=$(((n*m)**2))
+        # mu_fst_lst=$(((n*m)*5))
+        # mu_subm_1000=$(((n*m)*5))
+        # mu_subm_0001=$(((n*m)*5))
+        # mu_subm_0110=$(((n*m)*5))
+        # mu_restart=$(((n*m)*15))
+        # mu_glob_col=$(((n*m)*10))
+        # mu_change_class=$(((n*m)*10))
+
+        # # TEST 3
+        # mu_1c=$(((n*m)**2))
+        # mu_fst_lst=$(((n*m)*5))
+        # mu_subm_1000=$((0))
+        # mu_subm_0001=$((0))
+        # mu_subm_0110=$((0))
+        # mu_restart=$((0))
+        # mu_glob_col=$(((n*m)*30))
+        # mu_change_class=$(((n*m)*30))
+
+        # # TEST 4
+        # mu_1c=$(((n*m)**2))
+        # mu_fst_lst=$(((n*m)*5))
+        # mu_subm_1000=$((0))
+        # mu_subm_0001=$((0))
+        # mu_subm_0110=$((0))
+        # mu_restart=$((0))
+        # mu_glob_col=$(((n*m)*15))
+        # mu_change_class=$(((n*m)*15))
+
+        # # TEST 5 = TEST 4 MA con dwave
+
+        # # TEST 6
+        # mu_1c=$(((n*m)**2))
+        # mu_fst_lst=$(((n*m)*5))
+        # mu_subm_1000=$((0))
+        # mu_subm_0001=$((0))
+        # mu_subm_0110=$((0))
+        # mu_restart=$((0))
+        # mu_glob_col=$(((n*m)*40))
+        # mu_change_class=$(((n*m)*40))
+
+        # # TEST 7
+        # mu_mon=$((def_number * 20))
+        # mu_conc=$((n / m * 5))
+        # mu_thr=$((mu_conc))
+
+        # # TEST 8
+        # mu_mon=$((def_number * 20))
+        # mu_conc=$((n / m * 2))
+        # mu_thr=$((mu_conc * 5))
+        
+        # # TEST 8
+        # mu_mon=$((def_number * 15))
+        # mu_conc=$((n / m))
+        # mu_thr=$((mu_conc * 5))
+
+        # # TEST 9
+        # mu_mon=$((def_number * 15))
+        # mu_conc=$((1))
+        # mu_thr=$(((n/m) * 10))
+
+        # # TEST 9bis
+        # mu_mon=$((def_number * 15))
+        # mu_conc=$((0))
+        # mu_thr=$(((n/m) * 10))
+
+        # Nuovi test
+        # mu_mon=$((def_number * 10))
+        # mu_conc=$((3 * n / m))
+        # mu_thr=$((mu_conc / 2))
+        
+        # mu_1c=$(((n*m)**2))
+        # mu_fst_lst=$(((n*m)*5))
+        # mu_glob_col=$(((n*m)*40))
+        # mu_change_class=$(((n*m)*40))
+
+        # mu_subm_1000=$((0))
+        # mu_subm_0001=$((0))
+        # mu_subm_0110=$((0))
+        # mu_restart=$((0))
+
+        # # Nuovi test 2
+        # mu_mon=$((def_number * 12))
+        # mu_conc=$((3 * n / m))
+        # mu_thr=$((mu_conc / 2))
+
+        # # Nuovi test 3
+        # mu_1c=$(((n*m)**2))
+        # mu_fst_lst=$(((n*m)*5))
+        # mu_glob_col=$(((n*m)*50))
+        # mu_change_class=$(((n*m)*50))
+
+        # mu_mon=$((def_number * 12))
+        # mu_conc=$((3 * n / m))
+        # mu_thr=$((mu_conc / 2))
+
+        # Nuovi test 4
+        mu_1c=$(((n*m*2)**2))
         mu_fst_lst=$(((n*m)*5))
-        mu_subm_1000=$(((n*m)*5))
-        mu_subm_0001=$(((n*m)*5))
-        mu_subm_0110=$(((n*m)*5))
-        mu_restart=$(((n*m)*10))
-        mu_glob_col=$(((n*m)*6))
-        mu_change_class=$(((n*m)*6))
+        mu_glob_col=$(((n*m)*75))
+        mu_change_class=$(((n*m)*75))
+
+        mu_mon=$((def_number * 12))
+        mu_conc=$((3 * n / m))
+        mu_thr=$((mu_conc / 2))
+
+        # dwave
+        # mu_1c=$(((n*m)/4))
+        # mu_fst_lst=$(((n*m)*5))
+        # mu_glob_col=$(((n*m)*40))
+        # mu_change_class=$(((n*m)*40))
+        # mu_mon=$((def_number * 5))
+        # mu_conc=$((n / m * 10))
+        # mu_thr=$((mu_conc/2))
 
         # Set in config.yaml the correct parameters
         sed -i "19s/.*/n_counterpart: $n/" "$config_copy"
@@ -52,10 +169,11 @@ while IFS=";" read -r n m def1 def2 def3 sweeps; do
 
         echo "n: $n, m: $m, run $((run+1)), default=${default[run]}"
         # python cost_function.py "$config_copy" > "output/qubo_test3/test4_${n}_${m}_shots${sweeps}_run${run}.txt" 2>&1 &
-        python cost_function.py "$config_copy" > "output/qubo_test/test1_${n}_${m}_shots${shots}_run${run}.txt" 2>&1 &
+        python cost_function.py "$config_copy" > "output/new_qubo_test/test06_${n}_${m}_run${run}.txt" 2>&1 &
+
     done
     wait
 
 done < "$param_file"
 
-rm -f output/qubo_test/config_*_run*.yaml
+rm -f output/new_qubo_test/config_*_run*.yaml
